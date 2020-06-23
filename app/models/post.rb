ee395ce9dee95807ad69015c4dd49ccdb753eb9d
tyler jones <rteyl1690@gmail.com>
2020-06-23 01:13:37 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+  def clickbait
+    bait = ["Won't Believe", "Secret", "Top", "Guess"]
+    unless !title.nil? && bait.find{|w| title.include?(w)}
+      errors.add(:title, "need more clickbait-y title")
+    end
+  end
 end
