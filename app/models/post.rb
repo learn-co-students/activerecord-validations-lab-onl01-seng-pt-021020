962551d045df20719e5ad382617c028176ac6be4
Pablo Gonzalez <pabgon0@gmail.com>
2020-06-24 21:19:25 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :click_bait
+  
+  @@clickbait_words = [/Won't Believe/, /Secret/, /Top [number]/, /Guess/]
+  
+  def click_bait
+    
+    if @@clickbait_words.none? {|i| i.match title}
+      errors.add(:title, "not clickbait")
+    end
+  end
 end
